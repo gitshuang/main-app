d4d4c868bdd9c6bd157871da1cb24bed906a1797
lihs07 <lihs07@pipechina.com.cn>
2023-12-27 00:13:17 +0800

    主应用

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import { loadMicroApp } from 'qiankun';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  function goto(num) {
+    console.log(num,'=============================')
+    window.history.pushState(null, `/app${num}`, `/app${num}`)
+    // window.history.push( `/app${num}`)
+  }
+  // useEffect(()=>{
+  //   loadMicroApp({
+  //     name: 'app1',
+  //     entry: 'http://localhost:3001',
+  //     container: '#viewport',
+  //   });
+  // },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+       
+    //     <span  onClick={()=>{goto(1)}}>应用1</span>
+    //     <span  onClick={()=>{goto(2)}}>应用2</span>
+       
+    //   </header>
+    //   <div id="subapp-viewport"></div>
+    // </div>
+    
+    <Router>
+    <div>
+      <nav>
+        {/* 在主应用中通过 Link 触发路由跳转 */}
+        <Link to="/app1">Go to App 1</Link>
+        <Link to="/app2">Go to App 2</Link>
+        {/* 其他微应用的链接 */}
+      </nav>
+      <hr />
+      {/* 主应用中配置的路由规则 */}
+      <Routes>
+        <Route path="/app1"  />
+        <Route path="/app2"  />
+        {/* 其他微应用的路由规则 */}
+      </Routes>
+      <div id="viewport"></div>
     </div>
+  </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerMicroApps, start,setDefaultMountApp } from 'qiankun';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +16,58 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// 定义loader方法，loading改变时，将变量赋值给App.vue的data中的isLoading
+// function loader (loading) {
+//   if (instance && instance.$children) {
+//     // instance.$children[0] 是App.vue，此时直接改动App.vue的isLoading
+//     instance.$children[0].isLoading = loading
+//   }
+// }
+
+// 注册子应用
+registerMicroApps([
+  {
+    name: 'app1',
+    entry: 'http://localhost:3001',
+    container: '#viewport',
+    activeRule: '/app1',
+    props: {
+      routerBase: '/app1', // 下发基础路由
+      // getGlobalState: store.getGlobalState // 下发getGlobalState方法
+    },
+    // loader
+  },
+  {
+    name: 'app2',
+    entry: 'http://localhost:3002',
+    container: '#viewport',
+    activeRule: '/app2',
+    props: {
+      routerBase: '/app2', // 下发基础路由
+      // getGlobalState: store.getGlobalState // 下发getGlobalState方法
+    }
+  },
+],{
+  beforeLoad: app => {
+    console.log('before load app.name====>>>>>', app.name)
+  },
+  beforeMount: [
+    app => {
+      console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name)
+    }
+  ],
+  afterMount: [
+    app => {
+      console.log('[LifeCycle] after mount %c%s', 'color: green;', app.name)
+    }
+  ],
+  afterUnmount: [
+    app => {
+      console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name)
+    }
+  ]
+});
+
+setDefaultMountApp('app1')
+// 启动 qiankun
+start();
