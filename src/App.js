d0ef2a872e65c959c1d668f34e71362f4249a45a
lihs07 <lihs07@pipechina.com.cn>
2023-12-27 09:53:23 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 import './App.css';
 import { useEffect } from 'react';
 
 function App() {
-  function goto(num) {
-    console.log(num,'=============================')
-    window.history.pushState(null, `/app${num}`, `/app${num}`)
-  }
+  
 
   return (
     <Router>
     <div>
       <nav >
         {/* 在主应用中通过 Link 触发路由跳转 */}
-        <Link to="/app1" className='link'>Go to App 1</Link>
-        <Link to="/app2">Go to App 2</Link>
+        <NavLink to="/app1" className='link' >Go to App 1</NavLink>
+        <NavLink to="/app2" >Go to App 2</NavLink>
       </nav>
       <hr />
       {/* 主应用中配置的路由规则 */}
